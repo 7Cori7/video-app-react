49ac4bbf92c5caed29b9a78b7cdda63a0937d748
7Cori7 <corinaidldesign@gmail.com>
2024-03-27 16:02:56 -0400

    cambioooss

--- video-app/client/src/App.jsx
+++ video-app/client/src/App.jsx
@@ -2,8 +2,7 @@ import VideoApp from "./components/VideoApp.jsx";
 import Footer from "./components/footer.jsx";
 
 function App() {
-
-  //const url = import.meta.env.VITE_REACT_APP_API_URL;
+  
   const url = 'http://localhost:3000';
 
   return <>
